9e437c893214826c121b44a35edf9c078e15d69b
Tristan Krass <tristankrass@gmail.com>
2019-07-24 22:55:21 +0300

    finished initial tu

--- frontend/pages/blog.tsx
+++ frontend/pages/blog.tsx
@@ -1,24 +1,25 @@
 import Link from 'next/link';
 import Hoc from '../components/Hoc';
 interface IProps {
-  title: string;
+  id: string;
 }
 
 const PostLink = (props: IProps) => (
   <li>
-    <Link href={`/blogPage?title=${props.title}`}>
-      <a>{props.title}</a>
+    <Link href={`/p/[id]`} as={`/p/${props.id}`}>
+      <a>{props.id}</a>
     </Link>
   </li>
 );
+
 export default function Blog() {
   return (
     <Hoc>
       <h1>My Blog</h1>
       <ul>
-        <PostLink title="Hello Next.js" />
-        <PostLink title="Learn Next.js is awesome" />
-        <PostLink title="Deploy apps with Zeit" />
+        <PostLink id="hello-nextjs" />
+        <PostLink id="learn-next" />
+        <PostLink id="Deploy-apps" />
       </ul>
     </Hoc>
   );
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,53 @@
 import Hoc from '../components/Hoc';
-const Index = () => (
+import fetch from 'isomorphic-unfetch';
+import Link from 'next/link';
+
+const Index = (props: any) => (
   <Hoc>
-    <div>
-      <p>Hello Next.js</p>
-    </div>
+    <h1>My </h1>
+    <ul>
+      {props.shows.map((show: any) => (
+        <li key={show.id}>
+          <Link href="/p/[id]" as={`/p/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+    <style jsx>{`
+      h1,
+      a {
+        font-family: 'Arial';
+      }
+
+      ul {
+        padding: 0;
+      }
+
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </Hoc>
 );
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+  console.log(`Show data fetched. Count: ${data.length}`);
+  return {
+    shows: data.map((entry: any) => entry.show)
+  };
+};
 
 export default Index;
